6473a6b432aae328da37d42d855e1598f4ddac70
Mark Spanbroek <mark@spanbroek.net>
2020-03-17 09:49:10 +0100

    Add helper to convert key to array of bytes

--- sss.nim
+++ sss.nim
@@ -6,13 +6,16 @@ type
 
 proc `$`*(key: Key): string {.borrow.}
 
+template array*(key: Key): array[32, byte] =
+  array[32, byte](key)
+
 func randomKey*(): Key =
   assert(c.randombytes(addr result, uint(sizeof(result))) == 0)
 
 func shares*(key: Key, threshold, amount: static uint8): array[amount, Share] =
-  c.sss_create_keyshares(addr result[0], array[32, byte](key), amount, threshold)
+  c.sss_create_keyshares(addr result[0], key.array, amount, threshold)
 
 func combine*(shares: openArray[Share]): Key =
   let sharesPtr = unsafeAddr shares[0]
   let sharesLen = uint8(len(shares))
-  c.sss_combine_keyshares(array[32, byte](result), sharesPtr, sharesLen)
+  c.sss_combine_keyshares(result.array, sharesPtr, sharesLen)
